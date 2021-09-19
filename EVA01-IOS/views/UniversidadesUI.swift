c03b5f873ef39613323c0a02f50ea25f2243c126
Eduar Rugamas <juaneduardo021299@hotmail.com>
2021-09-19 15:06:56 -0600

    add files

---
+++ EVA01-IOS/views/UniversidadesUI.swift
@@ -0,0 +1,22 @@
+//
+//  UniversidadesUI.swift
+//  EVA01-IOS
+//
+//  Created by DEV EDUAR on 9/19/21.
+//
+
+import SwiftUI
+
+struct UniversidadesUI: View {
+    var body: some View {
+        VStack {
+
+        }
+    }
+}
+
+struct UniversidadesUI_Previews: PreviewProvider {
+    static var previews: some View {
+        UniversidadesUI()
+    }
+}
